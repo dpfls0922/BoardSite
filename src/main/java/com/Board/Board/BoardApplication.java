0edb0dff74cfff36708bf95c3af8f00e82f8f246
dpfls0922 <dpfls0922@naver.com>
2024-03-06 14:07:07 +0900

    [Add] put, delete 메소드 사용을 위한 코드 추가

--- src/main/java/com/Board/Board/BoardApplication.java
+++ src/main/java/com/Board/Board/BoardApplication.java
@@ -2,7 +2,10 @@ package com.Board.Board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+
 
 @EnableJpaAuditing
 @SpringBootApplication
@@ -12,4 +15,8 @@ public class BoardApplication {
 		SpringApplication.run(BoardApplication.class, args);
 	}
 
+	@Bean
+	public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+		return new HiddenHttpMethodFilter();
+	}
 }

79a48b8bb7b3c35bfe34c5ace58f4f0076e07423
dpfls0922 <dpfls0922@naver.com>
2024-03-08 13:50:00 +0900

    [Feat] Controller와 Service 테스트 코드 구현

---
+++ src/test/java/com/Board/Board/Controller/BoardControllerTest.java
@@ -0,0 +1,141 @@
+package com.Board.Board.Controller;
+
+import com.Board.Board.Domain.Entity.Board;
+import com.Board.Board.Dto.BoardDto;
+import com.Board.Board.Service.BoardService;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+@MockBean(JpaMetamodelMappingContext.class)
+@WebMvcTest(BoardController.class)
+@DisplayName("Controller 생성 테스트")
+class BoardControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;   // 서블릿 컨테니어 사용 x, 테스트용으로 Mvc 기능 사용할 수 있음
+
+    @MockBean
+    BoardService boardService; // 협력자
+
+
+    private List<Board> dummyBoards;
+    @BeforeEach
+    void beforeEach(){
+        dummyBoards = Arrays.asList(
+                new Board(1, "작성자 1", "이메일 1", "제목 1", "내용 1"),
+                new Board(2, "작성자 2", "이메일 2", "제목 2", "내용 2")
+        );
+    }
+
+    @Test
+    @DisplayName("목록 불러오기")
+    void list() throws Exception {
+        when(boardService.getAllBoardsReversed()).thenReturn(dummyBoards);
+
+        mockMvc.perform(get("/list"))
+                .andExpect(status().isOk())
+                .andExpect(model().attribute("boards", hasSize(dummyBoards.size())))
+                .andExpect(view().name("list"));
+    }
+
+    @Test
+    @DisplayName("글 상세보기")
+    void detail() throws Exception {
+        int boardId = 1;
+        BoardDto boardDto = new BoardDto();
+        when(boardService.getBoard(boardId)).thenReturn(boardDto);
+
+        mockMvc.perform(get("/list/{id}", boardId))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("board"))
+                .andExpect(view().name("article"));
+    }
+
+
+    @Nested
+    @DisplayName("글 등록하기")
+    class register {
+        @Test
+        @DisplayName("Get")
+        void registerGet() throws Exception {
+            mockMvc.perform(get("/register"))
+                    .andExpect(status().isOk())
+                    .andExpect(view().name("created"));
+        }
+
+        @Test
+        @DisplayName("Post")
+        void registerPost() throws Exception {
+            mockMvc.perform(post("/register")
+                    .contentType(MediaType.APPLICATION_FORM_URLENCODED) // application/x-www-form-urlencoded
+                    .param("title", "새로운 제목")
+                    .param("content", "새로운 내용"))
+                    .andExpect(status().is3xxRedirection())             // 리다이렉션 응답 코드
+                    .andExpect(redirectedUrl("/list"));
+
+            verify(boardService, times(1)).savePost(any());
+        }
+    }
+
+    @Nested
+    @DisplayName("글 수정하기")
+    class edit {
+        @DisplayName("Get")
+        @Test
+        void editGet() throws Exception {
+            int boardId = 1;
+            BoardDto boardDto = new BoardDto();
+            when(boardService.getBoard(boardId)).thenReturn(boardDto);
+
+            mockMvc.perform(get("/list/edit/{id}", boardId))
+                    .andExpect(status().isOk())
+                    .andExpect(model().attributeExists("board"))
+                    .andExpect(view().name("edit"));
+        }
+
+        @Test
+        @DisplayName("Put")
+        void editPut() throws Exception {
+            int boardId = 1;
+            mockMvc.perform(put("/list/edit/{id}", boardId)
+                            .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                            .param("name", "수정한 작성자")
+                            .param("title", "수정한 제목")
+                            .param("content", "수정한 내용"))
+                    .andExpect(status().is3xxRedirection())
+                    .andExpect(redirectedUrl("/list/1"));
+
+            verify(boardService, times(1)).savePost(any());
+        }
+    }
+
+    @Test
+    @DisplayName("글 삭제하기")
+    void deleteBoard() throws Exception {
+        int boardId = 1;
+        mockMvc.perform(delete("/list/{id]}", boardId))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/list"));
+
+        verify(boardService, times(1)).deletePost(boardId);
+    }
+}
--- src/test/java/com/Board/Board/Service/BoardServiceTest.java
+++ src/test/java/com/Board/Board/Service/BoardServiceTest.java
@@ -2,19 +2,24 @@ package com.Board.Board.Service;
 
 import com.Board.Board.Domain.Repository.BoardRepository;
 import com.Board.Board.Dto.BoardDto;
+import com.Board.Board.Domain.Entity.Board;
+
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import com.Board.Board.Domain.Entity.Board;
-import org.mockito.Mockito;
+
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.*;
 
 @Transactional
 @DisplayName("Service 생성 테스트")
@@ -27,22 +32,23 @@ class BoardServiceTest {
     BoardService boardService;
 
     @Test
-    @DisplayName("게시물 생성 테스트")
+    @DisplayName("글 생성하기")
     void savePostTest() {
         // given
         // 테스트 데이터 생성
         BoardDto boardDto = new BoardDto();
+        boardDto.setNum(1);
         boardDto.setName("yerin");
         boardDto.setSubject("title");
         boardDto.setContent("content");
         // 가짜 객체 생성 (실제 DB에 저장 안됨)
-        Board boardEntity =Board.builder()
+        Board boardEntity = Board.builder()
+                .num(boardDto.getNum())
                 .name(boardDto.getName())
                 .subject(boardDto.getSubject())
                 .content(boardDto.getContent())
                 .build();
-        // mocking
-        Mockito.when(boardRepository.save(Mockito.any())).thenReturn(boardEntity);
+        when(boardRepository.save(any())).thenReturn(boardEntity);
 
         // When
         Integer savedBoardNum = boardService.savePost(boardDto);
@@ -50,33 +56,89 @@ class BoardServiceTest {
         // Then
         assertEquals(boardEntity.getName(), boardDto.getName());
         assertEquals(boardEntity.getSubject(), boardDto.getSubject());
-//        assertEquals(1, savedBoardNum);
-//        assertNotNull(savedBoardNum, "게시물 번호는 null이 아니어야 합니다.");
-//        assertTrue(savedBoardNum > 0, "게시물 번호는 0보다 커야 합니다.");
+        assertEquals(1, savedBoardNum);
+        assertNotNull(savedBoardNum, "게시물 번호는 null이 아니어야 합니다."); // 실제로 디비에 저장되지 않기 때문에 setNum 하지 않으면 num은 null 값을 가짐
     }
 
     @Test
-    @DisplayName("getAllBoards 테스트")
+    @DisplayName("모든 게시글 불러오기")
     void getAllBoards() {
+        Board board1 = new Board();
+        Board board2 = new Board();
+
+        when(boardRepository.findAll()).thenReturn(Arrays.asList(board1, board2));
+
+        List<Board> boards = boardService.getAllBoards();
+
+        assertNotNull(boards, "게시물 목록은 null이 아니어야 합니다.");
+        assertEquals(2, boards.size());
     }
 
     @Test
-    @DisplayName("getAllBoardsReversed 테스트")
+    @DisplayName("모든 게시글 역순으로 불러오기")
     void getAllBoardsReversed() {
+        Board board1 = new Board();
+        Board board2 = new Board();
+
+        when(boardRepository.findAll()).thenReturn(Arrays.asList(board1, board2));
+
+        List<Board> reversedBoards = boardService.getAllBoardsReversed();
+
+        assertNotNull(reversedBoards, "게시물 목록은 null이 아니어야 합니다.");
+        assertEquals(2, reversedBoards.size());
+
+        // 검증: 역순으로 반환되었는지 확인
+        assert reversedBoards.get(0) == board2;
+        assert reversedBoards.get(1) == board1;
     }
 
     @Test
-    @DisplayName("getBoard 테스트")
+    @DisplayName("한 개의 게시글 불러오기")
     void getBoard() {
+        int boardId = 1;
+        Board board = new Board();
+        board.setNum(boardId);
+        board.setName("작성자");
+        board.setSubject("제목");
+        board.setContent("내용");
+
+        when(boardRepository.findById(boardId)).thenReturn(Optional.of(board));
+
+        BoardDto result = boardService.getBoard(boardId);
+
+        assertNotNull(result);
+        assertEquals(boardId, result.getNum());
+        assertEquals("작성자", result.getName());
+        assertEquals("제목", result.getSubject());
+        assertEquals("내용", result.getContent());
     }
 
     @Test
     @DisplayName("조회수 증가 테스트")
     void increaseHitCount() {
+        int boardId = 1;
+        Board board = new Board();
+        board.setNum(boardId);
+        board.setHitcount(5);
+
+        when(boardRepository.findById(boardId)).thenReturn(Optional.of(board));
+
+        boardService.increaseHitCount(boardId);
+
+        assertEquals(6, board.getHitCount());
+        verify(boardRepository, times(1)).findById(any());
     }
 
     @Test
     @DisplayName("게시글 삭제 테스트")
     void deletePost() {
+        int boardId = 1;
+
+        when(boardRepository.findById(boardId)).thenReturn(Optional.empty());
+
+        boardService.deletePost(boardId);
+
+        assertThrows(EntityNotFoundException.class, () -> boardService.getBoard(boardId));
+        verify(boardRepository, times(1)).deleteById(boardId);
     }
 }

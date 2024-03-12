1a4424cec37d88ed5efda81e7af859a6660d7f6d
dpfls0922 <dpfls0922@naver.com>
2024-03-12 18:28:47 +0900

    [Feat] 스프링 시큐리티 관련 설정

---
+++ src/main/java/com/Board/Board/Config/SecurityConfig.java
@@ -0,0 +1,70 @@
+package com.Board.Board.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(AbstractHttpConfigurer::disable)                   // cors 방지
+                .csrf(AbstractHttpConfigurer::disable)                   // csrf 방지
+                .formLogin(AbstractHttpConfigurer::disable)             // 기본 로그인 페이지 없애기
+                .headers((headers) -> headers
+                        .addHeaderWriter(new XFrameOptionsHeaderWriter(
+                                XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)));
+
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/admin").hasRole("ADMIN")
+                        .requestMatchers("/list/edit/**", "/register/**", "/edit/**").hasRole("MEMBER")
+                        .requestMatchers("/**", "/list", "/list/{id}", "/css/**", "/js/**").permitAll()
+                        .anyRequest().authenticated())
+                .formLogin((formLogin) -> formLogin
+                        .loginPage("/user/login")
+                        .defaultSuccessUrl("/list")
+                        .permitAll())
+                .logout((logout) -> logout
+                        .logoutRequestMatcher(new AntPathRequestMatcher("/user/logout"))
+                        .logoutSuccessUrl("/list")
+                        .invalidateHttpSession(true)
+                );
+
+        return http.build();
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AccessDeniedHandler accessDeniedHandler() {
+        return (request, response, accessDeniedException) -> {
+            // 사용자가 접근 권한이 없을 때
+            response.sendRedirect("/user/denied");
+        };
+    }
+}

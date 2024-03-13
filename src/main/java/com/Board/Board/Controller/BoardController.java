e7af5394238df901238980eeb278c1c8adc614d4
dpfls0922 <dpfls0922@naver.com>
2024-03-13 13:38:02 +0900

    [Chore] 주석 수정

--- src/main/java/com/Board/Board/Controller/BoardController.java
+++ src/main/java/com/Board/Board/Controller/BoardController.java
@@ -1,11 +1,13 @@
 package com.Board.Board.Controller;
 
 import com.Board.Board.Domain.Entity.Board;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
 import com.Board.Board.Dto.BoardDto;
 import com.Board.Board.Service.BoardService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,10 +24,12 @@ public class BoardController {
         this.boardService = boardService;
     }
 
-    // 전체 목록 뷸러오기
+    /*
+     * 게시글 전체 목록 뷸러오기
+     * @return 게시글 전체 목록 페이지
+     */
     @GetMapping("/list")
     public String list(Model model){
-        // 로깅
         List<Board> boards = boardService.getAllBoards();
         for (Board board : boards) {
             logger.info("Board: {}", board);
@@ -33,7 +37,12 @@ public class BoardController {
         model.addAttribute("boards", boardService.getAllBoardsReversed());
         return "board/list";
     }
-    // 상세 보기
+
+    /*
+     * 게시글 상세보기
+     * @param id 게시글 ID
+     * @return 게시글 상세보기 페이지
+     */
     @GetMapping("/list/{id}")
     public String detail(@PathVariable("id") int num, Model model) {
         boardService.increaseHitCount(num);
@@ -42,19 +51,31 @@ public class BoardController {
         return "board/article";
     }
 
-    // 생성
+    /*
+     * 게시글 작성
+     * @return 게시글 작성 페이지
+     */
     @GetMapping("/register")
     public String register(){
         return "board/created";
     }
 
+    /*
+     * 게시글 작성 post
+     * @param boardDto 게시글 작성 정보
+     * @return 게시글 목록 페이지
+     */
     @PostMapping("/register")
     public String register(BoardDto boardDto){
         boardService.savePost(boardDto);
         return "redirect:/list";
     }
 
-    // 수정
+    /*
+     * 게시글 수정
+     * @param id 게시글 ID
+     * @return 게시글 수정 페이지
+     */
     @GetMapping("/list/edit/{id}")
     public String edit(@PathVariable("id") int num, Model model) {
         BoardDto boardDto = boardService.getBoard(num);
@@ -62,6 +83,12 @@ public class BoardController {
         return "board/edit";
     }
 
+    /*
+     * 게시글 수정 post
+     * @param id 게시글 ID
+     * @param boardDto 게시글 수정 정보
+     * @return 게시글 디테일 페이지
+     */
     @PutMapping("/list/edit/{id}")
     public String edit(@PathVariable("id") int num, @ModelAttribute BoardDto boardDto) {
         boardDto.setNum(num);
@@ -69,8 +96,14 @@ public class BoardController {
         return "redirect:/list/{id}";
     }
 
-    @DeleteMapping("/list/{id}")
+    /*
+     * 게시글 삭제
+     * @param id 게시글 ID
+     * @return
+     */
+    @DeleteMapping("/list/remove/{id}")
     public String delete(@PathVariable("id") int num) {
+        // 로그인한 유저의 id와 작성자의 id가 일치해야 삭제 가능하도록 로직 추가하기
         boardService.deletePost(num);
         return "redirect:/list";
     }
--- src/main/java/com/Board/Board/Controller/MemberController.java
+++ src/main/java/com/Board/Board/Controller/MemberController.java
@@ -1,8 +1,11 @@
 package com.Board.Board.Controller;
 
+import ch.qos.logback.core.model.Model;
 import com.Board.Board.Dto.MemberDto;
 import com.Board.Board.Service.MemberService;
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;

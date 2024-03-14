f99f29cd072372537baeafec377d13f594fd8637
dpfls0922 <dpfls0922@naver.com>
2024-03-14 09:38:35 +0900

    [Feat] 유저 정보 불러오기, 수정 및 삭제 메소드 구현

--- src/main/java/com/Board/Board/Service/MemberService.java
+++ src/main/java/com/Board/Board/Service/MemberService.java
@@ -3,10 +3,15 @@ package com.Board.Board.Service;
 import com.Board.Board.Domain.Entity.Member;
 import com.Board.Board.Domain.Repository.MemberRepository;
 import com.Board.Board.Dto.MemberDto;
+
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Optional;
 
 @Service
 public class MemberService {
@@ -30,5 +35,45 @@ public class MemberService {
                 .password(memberDto.getPassword())
                 .build();
         return memberRepository.save(member).getId();
+    }
+
+    @Transactional
+    public MemberDto findMember(String userid) {
+        Optional<Member> memberOptional = memberRepository.findByUserid(userid);
+        Member member = memberOptional.orElseThrow(() -> new UsernameNotFoundException("아이디가 존재하지 않습니다"));
+
+        MemberDto memberDto = MemberDto.builder()
+                .member(member)
+                .build();
+
+        return memberDto;
+    }
+
+    public Long updateMember(MemberDto memberUpdatedDto) {
+        Member member = memberRepository.findByUserid(memberUpdatedDto.getUserid())
+                .orElseThrow(()->new UsernameNotFoundException("아이디가 존재하지 않습니다"));
+
+
+        memberUpdatedDto.setPassword(passwordEncoder.encode(memberUpdatedDto.getPassword()));
+
+        member.updateUsername(memberUpdatedDto.getUsername());
+        member.updateUserEmail(memberUpdatedDto.getEmail());
+        member.updatePassword(memberUpdatedDto.getPassword());
+
+        memberRepository.save(member);
+
+        return member.getId();
+    }
+
+    public boolean withdrawal(String userid, String password) {
+        Member member = memberRepository.findByUserid(userid)
+                .orElseThrow(() -> new UsernameNotFoundException("아이디가 존재하지 않습니다"));
+
+        if (passwordEncoder.matches(password, member.getPassword())) {
+            memberRepository.delete(member);
+            return true;
+        } else {
+            return false;
+        }
     }
 }

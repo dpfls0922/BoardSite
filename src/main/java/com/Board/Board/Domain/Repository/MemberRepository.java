33ce8fbf07659e8ccc7e13a9c67e88e0518fb3e2
dpfls0922 <dpfls0922@naver.com>
2024-03-14 18:29:16 +0900

    [Add] 유효성 검증에 사용할 메서드 추가

--- src/main/java/com/Board/Board/Domain/Repository/MemberRepository.java
+++ src/main/java/com/Board/Board/Domain/Repository/MemberRepository.java
@@ -6,5 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    /*
+    * 회원가입 시 유효성 체크
+    */
+    boolean existsByUserid(String userid);
+    boolean existsByEmail(String email);
     Optional<Member> findByUserid(String userid);
 }

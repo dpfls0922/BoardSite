5db90c2fa67dfd5dca1d8b3f93b3317678a46e8a
dpfls0922 <dpfls0922@naver.com>
2024-03-14 18:28:51 +0900

    [Add] findByMember 추가

--- src/main/java/com/Board/Board/Domain/Repository/BoardRepository.java
+++ src/main/java/com/Board/Board/Domain/Repository/BoardRepository.java
@@ -1,7 +1,10 @@
 package com.Board.Board.Domain.Repository;
 
 import com.Board.Board.Domain.Entity.Board;
+import com.Board.Board.Domain.Entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 public interface BoardRepository extends JpaRepository<Board, Integer> {
+    List<Board> findByMember(Member member);
 }

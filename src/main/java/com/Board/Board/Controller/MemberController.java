e886c7916ff848d0d2aff962248f01bcb531dbf2
dpfls0922 <dpfls0922@naver.com>
2024-03-14 09:41:11 +0900

    [Feat] 로그인한 유저 정보 불러오는 함수 및 회원정보 수정 및 탈퇴 메서드 구현

--- src/main/java/com/Board/Board/Controller/MemberController.java
+++ src/main/java/com/Board/Board/Controller/MemberController.java
@@ -1,15 +1,20 @@
 package com.Board.Board.Controller;
 
-import ch.qos.logback.core.model.Model;
 import com.Board.Board.Dto.MemberDto;
 import com.Board.Board.Service.MemberService;
-import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/user")
@@ -34,5 +39,63 @@ public class MemberController {
     @GetMapping("/login")
     public String login(){
         return "member/login";
+    }
+
+
+    @GetMapping("/profile")
+    public String mypage(Model model){
+        String userid = checkSession();
+        MemberDto member = memberService.findMember(userid);
+        model.addAttribute("member", member);
+        return "member/profile";
+    }
+
+    @GetMapping("/update")
+    public String updateUsernameForm(Model model){
+        String userid = checkSession();
+        MemberDto member = memberService.findMember(userid);
+        model.addAttribute("member", member);
+        return "member/updateProfile";
+    }
+
+    @PostMapping("/update")
+    public String updateUser(MemberDto memberUpdatedDto, Model model){
+        model.addAttribute("member", memberUpdatedDto);
+        memberService.updateMember(memberUpdatedDto);
+        return "member/profile";
+    }
+
+    @GetMapping("/withdrawal")
+    public String memberWithdrawal() {
+        return "member/withdrawal";
+    }
+
+    @PostMapping("/withdrawal")
+    public String memberWithdrawal(@RequestParam String password, Model model) {
+        boolean result = memberService.withdrawal(checkSession(), password);
+
+        if (result) {
+            return "redirect:/list";
+        } else {
+            model.addAttribute("wrongPassword", "비밀번호가 일치하지 않습니다");
+            return "member/withdrawal";
+        }
+    }
+    private static String checkSession() {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String userid = "";
+
+        if (principal instanceof UserDetails) {
+            UserDetails userDetails = (UserDetails) principal;
+
+            userid = userDetails.getUsername();
+            String auth = userDetails.getAuthorities().toString();
+
+            System.out.println("userid : " + userid);
+            System.out.println("auth : " + auth);
+        } else {
+            System.out.println("인증되지 않은 사용자입니다.");
+        }
+        return userid;
     }
 }

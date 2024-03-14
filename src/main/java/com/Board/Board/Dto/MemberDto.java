126205d6d04aa358445ca30bdb90240e3c837436
dpfls0922 <dpfls0922@naver.com>
2024-03-14 18:24:05 +0900

    [Refactor] 회원가입 Dto와 그 외의 경우에 사용하는 Dto로 분류

--- src/main/java/com/Board/Board/Dto/MemberDto.java
+++ src/main/java/com/Board/Board/Dto/MemberDto.java
@@ -1,9 +1,6 @@
 package com.Board.Board.Dto;
 
 import com.Board.Board.Domain.Entity.Member;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Getter
@@ -11,19 +8,9 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 public class MemberDto {
-    @NotBlank(message="이메일을 입력해주세요")
-    @Email(message = "올바른 이메일 주소를 입력해주세요")
     private String email;
-
-    @NotBlank(message="이름을 입력해주세요")
-    @Size(min = 2, max = 15, message = "2 ~ 15자 사이로 입력해주세요")
     private String username;
-
-    @NotBlank(message="아이디를 입력해주세요")
-    @Size(min = 5, max = 15, message = "5 ~ 15자 사이로 입력해주세요")
     private String userid;
-
-    @NotBlank(message="비밀번호를 입력해주세요")
     private String password;
 
     @Builder
---
+++ src/main/java/com/Board/Board/Dto/MemberSignupDto.java
@@ -0,0 +1,38 @@
+package com.Board.Board.Dto;
+
+import com.Board.Board.Domain.Entity.Member;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class MemberSignupDto {
+    @NotBlank(message="이메일을 입력해주세요")
+    @Email(message = "올바른 이메일 주소를 입력해주세요")
+    private String email;
+
+    @NotBlank(message="이름을 입력해주세요")
+    @Size(min = 2, max = 15, message = "2 ~ 15자 사이로 입력해주세요")
+    private String username;
+
+    @NotBlank(message="아이디를 입력해주세요")
+    @Size(min = 5, max = 15, message = "5 ~ 15자 사이로 입력해주세요")
+    private String userid;
+
+    @NotBlank(message="비밀번호를 입력해주세요")
+    private String password1;
+
+    @NotBlank(message="비밀번호를 입력해주세요")
+    private String password2;
+
+    @Builder
+    public MemberSignupDto(Member member) {
+        this.email = member.getEmail();
+        this.username = member.getUsername();
+        this.userid = member.getUserid();
+    }
+}

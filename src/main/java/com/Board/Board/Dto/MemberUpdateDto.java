f521307a9dcb9d196b71d160975aa6e5ca1ecdbc
dpfls0922 <dpfls0922@naver.com>
2024-03-15 17:47:11 +0900

    [Del] 사용하지 않는 빌더 제거

--- src/main/java/com/Board/Board/Dto/MemberUpdateDto.java
+++ src/main/java/com/Board/Board/Dto/MemberUpdateDto.java
@@ -1,16 +1,20 @@
 package com.Board.Board.Dto;
 
-import com.Board.Board.Domain.Entity.Member;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class MemberSignupDto {
+public class MemberUpdateDto {
+    private String userid;
+
     @NotBlank(message="이메일을 입력해주세요")
     @Email(message = "올바른 이메일 주소를 입력해주세요")
     private String email;
@@ -19,13 +23,6 @@ public class MemberSignupDto {
     @Size(min = 2, max = 15, message = "2 ~ 15자 사이로 입력해주세요")
     private String username;
 
-    @NotBlank(message="아이디를 입력해주세요")
-    @Size(min = 5, max = 15, message = "5 ~ 15자 사이로 입력해주세요")
-    private String userid;
-
     @NotBlank(message="비밀번호를 입력해주세요")
-    private String password1;
-
-    @NotBlank(message="비밀번호를 입력해주세요")
-    private String password2;
+    private String password;
 }

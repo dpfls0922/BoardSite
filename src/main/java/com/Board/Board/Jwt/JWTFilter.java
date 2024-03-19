ae81c671b5313240418ea56aea3d481313ec86ee
dpfls0922 <dpfls0922@naver.com>
2024-03-19 16:53:00 +0900

    [Feat] JWT 검증 필터 구현

---
+++ src/main/java/com/Board/Board/Jwt/JWTFilter.java
@@ -0,0 +1,59 @@
+package com.Board.Board.Jwt;
+
+import com.Board.Board.Domain.Entity.Member;
+import com.Board.Board.Domain.Entity.MemberRole;
+import com.Board.Board.Dto.MemberDetails;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class JWTFilter extends OncePerRequestFilter {
+    private final JWTUtil jwtUtil;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authorization = request.getHeader("Authorization");
+
+        if (authorization == null || !authorization.startsWith("Bearer ")){
+            System.out.println("token null");
+            filterChain.doFilter(request, response);
+
+            return;
+        }
+
+        System.out.println("authorization now");
+        String token = authorization.split(" ")[1];
+
+        if (jwtUtil.isExpired(token)){
+            System.out.println("token expired");
+            filterChain.doFilter(request, response);
+
+            return;
+        }
+
+        String username = jwtUtil.getUsername(token);
+        String role = jwtUtil.getRole(token);
+
+        Member member = Member.builder()
+                        .userid(username)
+                        .password("temppassword")
+                        .role(MemberRole.valueOf(role))
+                        .build();
+
+        MemberDetails memberDetails = new MemberDetails(member);
+
+        Authentication authToken = new UsernamePasswordAuthenticationToken(memberDetails, null, memberDetails.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(authToken);
+
+        filterChain.doFilter(request, response);
+    }
+}

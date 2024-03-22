012aaf4c3fcd1a27e21f60a86cddd047e78dbb2e
dpfls0922 <dpfls0922@naver.com>
2024-03-22 18:29:27 +0900

    [Feat] cookie 생성 및 token 가져오기 함수 구현

--- src/main/java/com/Board/Board/Jwt/JWTUtil.java
+++ src/main/java/com/Board/Board/Jwt/JWTUtil.java
@@ -1,11 +1,16 @@
 package com.Board.Board.Jwt;
 
 import io.jsonwebtoken.Jwts;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
+import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
@@ -40,5 +45,36 @@ public class JWTUtil {
                 .expiration(new Date(System.currentTimeMillis() + expiredMs))
                 .signWith(secretKey)
                 .compact();
+    }
+    public void addJwtToCookie(String token, HttpServletResponse response) {
+        Cookie cookie = new Cookie("jwt", token);
+        cookie.setHttpOnly(true);
+        cookie.setPath("/"); // 모든 곳에서 쿠키열람이 가능
+        response.addCookie(cookie);
+    }
+
+    // header 토큰을 가져오기 -> 헤더검사 없으면 쿠키검사
+    public String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+
+        /*헤더에 값이 없다면 토큰 확인*/
+        if (bearerToken == null) {
+            Cookie[] cookies = request.getCookies();
+            if (cookies != null) {
+                for (Cookie c : cookies) {
+                    String name = c.getName();
+                    String value = c.getValue();
+                    if (name.equals("jwt")) {
+                        bearerToken = value;
+                        return bearerToken;
+                    }
+                }
+            }
+        } else{
+            if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
+                return bearerToken.substring(7);
+            }
+        }
+        return null;
     }
 }

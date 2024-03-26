bc5f4b3754310b26c1a6e497a1c9b026f1dbc188
dpfls0922 <dpfls0922@naver.com>
2024-03-26 11:03:49 +0900

    [Chore] jwt 만료 시 Exception 처리

--- src/main/java/com/Board/Board/Jwt/JWTFilter.java
+++ src/main/java/com/Board/Board/Jwt/JWTFilter.java
@@ -3,6 +3,8 @@ package com.Board.Board.Jwt;
 import com.Board.Board.Domain.Entity.Member;
 import com.Board.Board.Domain.Entity.MemberRole;
 import com.Board.Board.Dto.MemberDetails;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -21,30 +23,33 @@ public class JWTFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-
-        System.out.println("JWTFilter now");
         String token = jwtUtil.resolveToken(request);
 
-        System.out.println("token : " + token);
-
         if (token != null) {
-            if (jwtUtil.isExpired(token)) {
-                System.out.println("token expired");
+            try {
+                jwtUtil.isExpired(token);
+            } catch(ExpiredJwtException e) {
+                logger.info("token expired");
+                throw new JwtException("만료된 JWT 토큰입니다", e);
             }
+
             String username = jwtUtil.getUsername(token);
+            MemberRole role;
+            if ("admin".equals(username)) {
+                role = MemberRole.ADMIN;
+            } else {
+                role = MemberRole.USER;
+            }
             Member member = Member.builder()
                     .userid(username)
                     .password("temppassword")
-                    .role(MemberRole.USER)
+                    .role(role)
                     .build();
 
             MemberDetails memberDetails = new MemberDetails(member);
 
             Authentication authToken = new UsernamePasswordAuthenticationToken(memberDetails, null, memberDetails.getAuthorities());
             SecurityContextHolder.getContext().setAuthentication(authToken);
-
-            System.out.println("Authenticated");
-            System.out.println("token : " + token);
         }
 
         /*다음 필터 진행*/

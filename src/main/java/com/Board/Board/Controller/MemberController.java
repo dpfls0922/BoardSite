2abfd021a023f68a0b53fd47b37b30eb3b886450
dpfls0922 <dpfls0922@naver.com>
2024-03-27 12:16:43 +0900

    [Fix] initBinder에 검증할 모델 객체 지정

--- src/main/java/com/Board/Board/Controller/MemberController.java
+++ src/main/java/com/Board/Board/Controller/MemberController.java
@@ -8,6 +8,7 @@ import com.Board.Board.Validator.CheckSignupValidator;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
@@ -21,7 +22,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import jakarta.servlet.http.HttpServletRequest;
 
-
+@Slf4j
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/user")
@@ -30,7 +31,7 @@ public class MemberController {
     private final CheckSignupValidator checkSignupValidator;
 
     /* 유효성 검증 */
-    @InitBinder
+    @InitBinder("MemberSignupDto")
     public void validatorBinder(WebDataBinder binder) {
         binder.addValidators(checkSignupValidator);
     }
